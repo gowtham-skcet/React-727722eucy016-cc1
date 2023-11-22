334041b3f5340537d89e69a2684d560f8663496f
gowtham-skcet <151620496+gowtham-skcet@users.noreply.github.com>
2023-11-22 12:41:41 +0530

    Add files via upload
    hi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Font from "./Font_size";
+import Weather from './Weather'
+const App = () => {
+  return (
+    <div>
+      <Font />
+      <Weather name="Coimbatore"/>
+    </div>
+  );
+};
+
+export default App;
